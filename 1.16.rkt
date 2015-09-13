74c5f122181c4e348b57550d4658de27ec714b1f
nubnokot <nubnokot@gmail.com>
2015-09-13 18:31:13 +0300

    add 1.16

---
+++ 1.16.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (fast-expt b n)
+  
+  (define (even? t)
+    (= (remainder t 2) 0))
+  
+  (define (iter a expt new-n counter)
+    (if (< counter n)
+        (if (even? new-n)
+        (iter a (* expt expt) (/ new-n 2) (* counter 2))
+        (iter (* a b) expt (- new-n 1) (+ 1 counter)))
+        (* a expt)))
+  (iter 1 b n 1))
+
+(fast-expt 3 5)
