d6431c6bcdf50ec807b8bedbf8dab9809b17b175
nubnokot <nubnokot@gmail.com>
2015-09-19 16:53:40 +0300

    Now works

--- 2.18.rkt
+++ 2.18.rkt
@@ -1,21 +1,20 @@
 #lang racket
 
-(define (append list1 list2)
-  (if (null? list1)
-      list2
-      (cons (car list1) (append (cdr list1) list2))))
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
 
-(define (last-pair s)
-  (if (null? (cdr s))
-      (car s)
-      (last-pair (cdr s))))
+(define (listref items n)
+      (if (= n 0)
+          (car items)
+          (listref (cdr items) (- n 1))))
 
 (define (reverse s)
-  (if (null? (cdr s))
-      (car s)
-      (list (reverse(cdr s)) (car s))))
-
-(reverse (list 1 2 3 4))
-  (reverse (list 1 2 3 4 5))
-       
-      
+  
+  (define (iter n)
+    (if (= n -1)
+       '()
+       (cons (listref s n) (iter (- n 1)))))
+  
+  (iter (- (length s) 1)))     
