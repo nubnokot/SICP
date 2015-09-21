1fef948d9498c1ad734078980403e897adcce964
nubnokot <nubnokot@gmail.com>
2015-09-21 17:53:44 +0300

    Add 2.32

---
+++ 2.32.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(define (identy x)
+  x)
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
